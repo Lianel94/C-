72b9b818638b61b850f35b09d91b50a14e8756f8
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-10 22:36:55 +0500

    example_005 conditions_if_else

---
+++ example_005_ifElseCondition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Type user name");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, it's Masha!!!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
