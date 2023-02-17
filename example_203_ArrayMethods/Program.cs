867c4d2ab9aeb8c4698e5169f390c27c47867a1e
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-17 15:47:23 +0500

    example_203 Array Methods

---
+++ example_203_ArrayMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {10, 54, 35 , 54, 58, 62, 77, 98};
+
+int n = array.Length;
+int find = 54;
+
+int index = 0;
+
+while(index < n)
+{
+	if(array[index] == find)
+	{
+		Console.WriteLine(index);
+		break; // will find only the first element
+	}
+	index++;
+}
