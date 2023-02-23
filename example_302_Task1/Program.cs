147a7372a5a7527a071ad76503188b0286707e5b
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-02-23 13:47:01 +0500

    THIRD LESSON task 1

---
+++ example_302_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Текст. заменить пробелы черточками, маленькие буквы к - на большие К, а большие С - на маленикие с
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля."
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldvalue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for(int i = 0; i < lenght; i++)
+    {
+        if(text[i]== oldvalue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+
